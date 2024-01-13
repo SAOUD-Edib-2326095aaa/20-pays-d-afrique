8c21b46bbd7e337045ef8bf076f809cf5eabb287
SAOUD-Edib-2326095aaa <edib.saoud@etu.univ-amu.fr>
2024-01-13 10:24:56 +0100

    egrgegr

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  var isFirstLoad = sessionStorage.getItem('firstLoad') === null;
+
+
+  if (isFirstLoad) {
+    var isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+    if (isDarkTheme) {
+      document.body.classList.add('dark-theme');
+    }
+  }
+
+
+  sessionStorage.setItem('firstLoad', 'false');
+});
+
 function toggleTheme() {
-
   var body = document.body;
-
-
   body.classList.toggle('dark-theme');
-
-
   var isDarkTheme = body.classList.contains('dark-theme');
   localStorage.setItem('darkTheme', isDarkTheme);
 }
-
-
-window.addEventListener('DOMContentLoaded', function () {
-  var isDarkTheme = localStorage.getItem('darkTheme') === 'true';
-  if (isDarkTheme) {
-    document.body.classList.add('dark-theme');
-  }
-});
